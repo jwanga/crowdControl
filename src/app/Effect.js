b87e536adc1697737d5adb28c2778b1f0ffee049
James Wanga <james@thedigitalself.com>
2016-10-29 16:21:47 -0700

    added effect class

---
+++ src/app/Effect.js
@@ -0,0 +1,10 @@
+/**
+ * The Effect class is a node in a singly linked list that defines the "time" and "severity" of an effect.
+ */
+class Effect {
+  constructor() {
+    this.time = 0;
+    this.severity = 0;
+    this.next = null;
+  }
+}
