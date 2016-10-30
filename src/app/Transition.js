650622baff5ed54af1a7ac0f3324161c3351a4cc
James Wanga <james@thedigitalself.com>
2016-10-30 16:28:48 -0700

    created linked list state machine and test cases

--- src/app/Transition.js
+++ src/app/Transition.js
@@ -1,10 +1,142 @@
 /**
- * The Transition class is a node in a singly linked list that defines the time an effect severity transition.
+ * The Transition class is a node in a linked list that defines the time of a severity transition.
  */
 class Transition {
-  constructor(time = 0, severity = 0) {
+
+  constructor(time = 0, duration = 0, severity = 0, previous = null, next = null) {
     this.time = time;
+    this.duration = duration;
     this.severity = severity;
-    this.next = null;
+    this.previous = previous;
+    this.next = next;
+    
+    //All new transitions should be proceded by a transition back to 0.
+    if (duration !== 0 && !next) {
+      this.next = new Transition(this.time + this.duration, 0, 0, this);
+    }
+  }
+
+  /**
+   * Compares the passed transition to the current instance and appends it appropriately.
+   * @param {Transition} - The transition to append.
+   */
+  append(transition) {
+    
+    if (transition.time === this.time 
+        && (transition.time + transition.duration) === (this.time + this.duration)
+        && transition.severity > this.severity) {
+      //The passed transition starts and ends at the same time and has a greater severity.
+
+      this.previous.next = transition;
+
+    } else if (transition.time === this.time 
+        && (transition.time + transition.duration) > (this.time + this.duration)
+        && transition.severity > this.severity) {
+      //The passed transition starts at the same time, ends later, and has a greater and severity.
+
+      if (!this.previous) {
+        this.previous = this;
+      }
+
+      this.previous.next = transition.severity != this.previous.severity 
+                          ? transition 
+                          : transition.next; 
+      
+    } else if (transition.time === this.time 
+        && (transition.time + transition.duration) > (this.time + this.duration)
+        && transition.severity === this.severity) {
+      //The passed transition starts at the same time, ends later, and has the same sevarity.
+
+      this.previous.next = transition;
+      
+    } else if (transition.time === this.time 
+        && (transition.time + transition.duration) > (this.time + this.duration)
+        && transition.severity < this.severity) {
+      //The passed transition starts at the same time , ends later, and has a lesser sevarity.
+
+      const newTransition = new Transition(this.time + this.duration, 
+                                  (this.time + this.duration) - (transition.time + transition.duration),
+                                  transition.severity, 
+                                  this, 
+                                  transition.next);
+      this.next = newTransition;
+      
+    } else if (transition.time === this.time 
+        && (transition.time + transition.duration) < (this.time + this.duration) 
+        && transition.severity > this.severity) {
+      //The passed transition starts at the same time, ends earlier, and has a greater sevarity.
+
+      const newTransition = new Transition(transition.time + transition.duration, 
+                                  (this.time + this.duration) - (transition.time + transition.duration),
+                                  this.severity, 
+                                  transition, 
+                                  this.next);
+        
+      transition.next = newTransition;
+      this.previous.next = transition;
+      
+    } else if (transition.time > this.time 
+        && (transition.time + transition.duration) === (this.time + this.duration)
+        && transition.severity > this.severity) {
+      //The passed transition starts later, ends at the same time, and has a greater sevarity.
+
+      if(this.next.next){
+        transition.next = this.next.next;
+      }
+
+      this.next = transition;
+      
+    } else if (transition.time > this.time 
+        && (transition.time + transition.duration) < (this.time + this.duration)
+        && transition.severity > this.severity) {
+       //The passed transition starts later, ends before, and has a greater sevarity.
+
+      const newTransition = new Transition(transition.time + transition.duration, 
+                                  (this.time + this.duration) - (transition.time + transition.duration),
+                                  this.severity, 
+                                  transition, 
+                                  this.next);
+        
+      transition.next = newTransition;
+      this.next = transition;
+      
+    } else if (transition.time > this.time 
+        && transition.time < (this.time + this.duration)
+        && (transition.time + transition.duration) > (this.time + this.duration)
+        && transition.severity >= this.severity) {
+      //The passed transition starts before the currrent transition ends, ends after and has a greater or equal sevarity.
+
+      this.next = transition.severity > this.severity 
+                  ? transition
+                  : transition.next;
+
+    } else if (transition.time > this.time 
+        && transition.time < (this.time + this.duration)
+        && (transition.time + transition.duration) > (this.time + this.duration)
+        && transition.severity < this.severity) {
+      //The passed transition starts before the currrent transition ends, ends after and has a lesser sevarity.
+
+      if (this.next.next) {
+        transition.next.append(this.next.next);
+      }
+
+      const newTransition = new Transition(this.time + this.duration, 
+                                    (transition.time + transition.duration) - (this.time + this.duration),
+                                    transition.severity, 
+                                    this, 
+                                    transition.next);
+                                    
+      this.next = newTransition;
+      
+    } else if (transition.time >= (this.time + this.duration)) {
+      //The passed transition starts after the current transition ends.
+
+      transition.previous = this;
+      if (!this.next) {
+        this.next = transition;
+      } else {
+        this.next.append(transition);
+      }
+    }
   }
 }
--- src/app/crowdControlCalc.js
+++ src/app/crowdControlCalc.js
@@ -10,8 +10,15 @@ const crowdControlCalc = (function() {
   class Calculator {
 
     constructor() {
+      this._reset();
+    }
+
+    /**
+     * Resets the transitions to [[0, 0]].
+     */
+    _reset() {
       //Singly linked list of effect transitions.
-      this.output = new Transition();
+      this.transitions = new Transition();
     }
 
     /**
@@ -21,18 +28,27 @@ const crowdControlCalc = (function() {
      */
     calculateCrowdControlChange( input ) {
       
+      //Assert the input is a valid array.
       if (!Array.isArray(input)) {
         throw new Error('"calculateCrowdControlChange" must be called with an array');
       }
 
-      input.forEach((effect) => {
-        if (!Array.isArray(effect)){
+      this._reset();
 
+      input.forEach((effect, index) => {
+
+        //Assert the effect array is a vaid collection of three number values.
+        if (!Array.isArray(effect) || effect.length != 3 || typeof(effect[0]) !== 'number' || typeof(effect[1]) !== 'number' || typeof(effect[2]) !== 'number' ) {
+          throw new Error(`Input index: ${index} must be an array of 3 numbers`);
         }
-        //this.output
+
+        //Append the new transition.
+        const transition = new Transition(...effect);
+        this.transitions.append(transition);
+
       });
 
-      console.log('foo2', JSON.stringify(input), JSON.stringify(this.toArray()));
+      return this.toArray();
     }
 
     /**
@@ -40,13 +56,18 @@ const crowdControlCalc = (function() {
      * @returns {array} 2D array of effect transitions.
      */
     toArray() {
-      let node = this.output;
+      let node = this.transitions;
       // 2D array of transitions.
       let output = [];
+      while(node) {
 
-      while(node.next) {
-        output.push([node.time, node.severity]);
+        //Don't add the first node if it has no severity.
+        if (!(node.time === 0 && node.severity === 0)){
+          output.push([node.time, node.severity]);
+        }
+
         node = node.next;
+
       }
 
       return output;
@@ -56,5 +77,94 @@ const crowdControlCalc = (function() {
   return new Calculator();
 }());
 
-crowdControlCalc.calculateCrowdControlChange([[1, 2, 3], [2, 3, 3], [1, 5, 3], [6, 2, 3], [6, 1, 3]]);
-crowdControlCalc.calculateCrowdControlChange();
+const effects = [ 
+  {
+    effect: [[1, 2, 5], [2, 3, 3], [4, 2, 4]],
+    result: [[1, 5], [3, 3], [4, 4], [6, 0]]
+  },
+  {
+    effect: [[0, 2, 3], [2, 3, 4], [5, 2, 2]],
+    result: [[0, 3], [2, 4], [5, 2], [7, 0]]
+  },  
+  {
+    effect: [[2, 4, 5], [8, 1, 1], [3, 4, 2], [4, 1, 6]],
+    result: [[2, 5], [4, 6], [5, 5], [6, 2], [7, 0], [8, 1], [9, 0]]
+  },
+  {
+    effect: [[1, 2, 3], [2, 3, 3], [1, 5, 3], [6, 2, 3], [6, 1, 3]],
+    result: [[1, 3], [8, 0]]
+  },
+  {
+    effect: [[1, 3, 3], [4, 1, 3]],
+    result: [[1, 3], [5,0]]
+  },
+  {
+    effect: [[1, 3, 3], [2, 1, 4]],
+    result: [[1, 3], [2, 4], [3, 3], [4, 0]]
+  },
+  {
+    effect: [[1, 3, 3], [2, 1, 2]],
+    result: [[1, 3], [4, 0]]
+  },
+  {
+    effect: [[1, 3, 3], [2, 1, 3]],
+    result: [[1, 3], [4, 0]]
+  },
+  {
+    effect: [[1, 2, 3], [2, 2, 2]],
+    result: [[1, 3], [3, 2], [4, 0]]
+  },
+  {
+    effect: [[1, 2, 3], [2, 2, 3]],
+    result: [[1, 3], [4, 0]]
+  },
+  {
+    effect: [[1, 2, 3], [2, 1, 2]],
+    result: [[1, 3], [3, 0]]
+  },
+  {
+    effect: [[1, 2, 3], [2, 1, 3]],
+    result: [[1, 3], [3, 0]]
+  },
+  {
+    effect: [[1, 2, 3], [1, 3, 2]],
+    result: [[1, 3], [3, 2], [4, 0]]
+  },
+  {
+    effect: [[1, 2, 3], [1, 3, 3]],
+    result: [[1, 3], [4, 0]]
+  },
+  {
+    effect: [[1, 2, 3], [1, 3, 4]],
+    result: [[1, 4], [4, 0]]
+  },
+  {
+    effect: [[1, 2, 3], [1, 1, 4]],
+    result: [[1, 4], [2, 3], [3, 0]]
+  },
+  {
+    effect: [[1, 2, 3], [1, 2, 4]],
+    result: [[1, 4], [3, 0]]
+  },
+  {
+    effect: [[1, 2, 3], [1, 2, 3]],
+    result: [[1, 3], [3, 0]]
+  },
+  {
+    effect: [[1, 2, 3], [1, 2, 3]],
+    result: [[1, 3], [3, 0]]
+  },
+  {
+    effect: [[1, 2, 3]],
+    result: [[1, 3], [3, 0]]
+  }
+];
+
+effects.forEach((effect) => {
+  const result = crowdControlCalc.calculateCrowdControlChange(effect.effect);
+
+  console.log(JSON.stringify(effect.effect));
+  console.log(JSON.stringify(result));
+  console.log(JSON.stringify(result) === JSON.stringify(effect.result));
+  console.log('-----------------------------------------------------------');
+});
